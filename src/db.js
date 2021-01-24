ada07fe144b3abe1fa6d3e8ab03570d48507dc97
ezeszuchet <szuchetezequiel@gmail.com>
2021-01-24 17:40:59 -0300

    Sql connection

--- src/db.js
+++ src/db.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql');
 
-//sql connection
-const con = mysql.createConnection({
+//sql connection local
+/*const con = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: '',
     database: 'pshgame'
-});
+});*/
+
+//sql connection production
+const con = mysql.createConnection('mysql://n41n45c9dkn2jbu9:wc3qhh7j0ax8s6c5@cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/smdnzbhbqupmymen');
+
+
 
 con.connect(function (err) {
     if (err) throw err;
