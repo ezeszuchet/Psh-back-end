0a46a58e7865e1341b1ab79e21b6f1da22bdd76b
ezeszuchet <szuchetezequiel@gmail.com>
2021-01-24 21:06:43 -0300

    Give permisions to request

--- src/index.js
+++ src/index.js
@@ -15,7 +15,11 @@ app.use('/players', playerRoutes);
 app.use('/stats', statRoutes);
 
 app.get('/', function (req, res) {
-    res.send('Server is running on port 2000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    res.send('Server is running');
 });
 
 const PORT = process.env.PORT || 5001;
